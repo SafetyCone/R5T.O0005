a9607a41c05fa805fb0d73d0452fd2633f3174e7
davidcoats <david@getsafetycone.com>
2023-06-01 18:06:55 -0700

    Interim changes.

--- source/R5T.O0005/Code/Instances.cs
+++ source/R5T.O0005/Code/Instances.cs
@@ -5,6 +5,6 @@ namespace R5T.O0005
 {
     public static class Instances
     {
-		
+        public static F0020.IProjectFileOperator ProjectFileOperator => F0020.ProjectFileOperator.Instance;
     }
 }
---
+++ source/R5T.O0005/Code/Values/IProjectOperations.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using R5T.T0131;
+using R5T.T0172;
+using R5T.T0179.Extensions;
+using R5T.T0195;
+
+
+namespace R5T.O0005
+{
+    [ValuesMarker]
+    public partial interface IProjectOperations : IValuesMarker
+    {
+        public F0056.IProjectOperations StringlyTyped => F0056.ProjectOperations.Instance;
+
+
+        public async Task Add_ProjectReferences(
+            IProjectFilePath projectFilePath,
+            IEnumerable<IProjectFileReference> projectFileReferences)
+        {
+            var values = projectFileReferences.Get_Values();
+
+            await Instances.ProjectFileOperator.AddProjectReferences_Idempotent(
+                projectFilePath.Value,
+                values);
+
+            await StringlyTyped.UpdateSolutions(
+                projectFilePath.Value);
+        }
+
+        public Task Add_ProjectReferences(
+            IProjectFilePath projectFilePath,
+            params IProjectFileReference[] projectFileReferences)
+        {
+            return this.Add_ProjectReferences(
+                projectFilePath,
+                projectFileReferences.AsEnumerable());
+        }
+
+        public Task Add_ProjectReference(
+            IProjectFilePath projectFilePath,
+            IProjectFileReference projectFileReference)
+        {
+            return this.Add_ProjectReferences(
+                projectFilePath,
+                projectFileReference);
+        }
+    }
+}
